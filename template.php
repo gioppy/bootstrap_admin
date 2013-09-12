704e6c519dfc37399e7166ef86f53d009829ae8f
Giovanni Buffa <giova.buffa@gmail.com>
2013-09-12 15:39:45 +0200

    Developer release

---
+++ template.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file template.php
+ */
+
+// Provide < PHP 5.3 support for the __DIR__ constant.
+if (!defined('__DIR__')) {
+  define('__DIR__', dirname(__FILE__));
+}
+require_once __DIR__ . '/includes/form.inc';
