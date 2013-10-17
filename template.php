ef42079932f21058c944fdd51890874a986bd5f8
Giovanni Buffa <giova.buffa@gmail.com>
2013-10-17 13:16:21 +0200

    Add support for Shortcut link
    Add support for "Add Shortcut" link in admin pages, if the Shortcut
    module is enable

--- template.php
+++ template.php
@@ -10,3 +10,30 @@ if (!defined('__DIR__')) {
 }
 require_once __DIR__ . '/includes/form.inc';
 require_once __DIR__ . '/includes/block.inc';
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function bootstrap_admin_preprocess_page(&$variables){
+  // Only display the shortcut link if the user has the ability to edit
+  // shortcuts and if the page's actual content is being shown (for example,
+  // we do not want to display it on "access denied" or "page not found"
+  // pages).
+  if (shortcut_set_edit_access() && ($item = menu_get_item()) && $item['access']) {
+    if (theme_get_setting('shortcut_module_link')) {
+      $shortcut_link = $variables['title_suffix']['add_or_remove_shortcut'];
+      $link_text = strip_tags($shortcut_link['#title'], '<em>');
+      $link_path = $shortcut_link['#href'];
+      $query = $shortcut_link['#options']['query'];
+      
+      unset($variables['title_suffix']['add_or_remove_shortcut']['#attached']['css']);
+      
+      $variables['title_suffix']['add_or_remove_shortcut'] = array(
+        '#prefix' => '<div class="add-or-remove-shortcuts"><div class="btn-group dropup"><button class="btn btn-primary btn-mini">' . t('Add shortcut') . '</button><button class="btn dropdown-toggle btn-primary btn-mini" data-toggle="dropdown"><span class="caret"></span></button>',
+        '#type' => 'markup',
+        '#markup' => '<ul class="dropdown-menu"><li>' . l($link_text, $link_path, array('query' => $query, 'html' => TRUE)) . '</li></ul>',
+        '#suffix' => '</div></div>',
+      );
+    }
+  }
+}
